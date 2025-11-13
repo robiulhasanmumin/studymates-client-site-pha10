2068c7ba85763965605aeb9553daf27a948fc558
robiulhasanmumin <ot7mumin@gmail.com>
2025-11-14 01:39:35 +0600

    Login section design done.

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,7 +2,90 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <motion.div initial={{ opacity: 0, y: 100 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ duration: 0.7, ease: "easeOut" }} className=" bg-base-200 py-20">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <h1 className="text-2xl font-bold text-center mt-5">Login</h1>
+          <div className="card-body">
+            <form onSubmit={handleLogin} className="fieldset">
+              {/* email */}
+              <label className="label">Email</label>
+              <input
+                type="email"
+                className="input"
+                name="email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                placeholder="Email"
+                required
+              />
+              {/* password */}
+              <label className="label">Password</label>
+              <input
+                type="password"
+                className="input"
+                name="password"
+                placeholder="Password"
+                required
+              />
+              <p className="text-red-500">{err && err}</p>
+              <div>
+                <Link to="/forgotPassword" state={{email}} className="link link-hover ">
+                  Forgot password?
+                </Link>
+              </div>
+              
+              <button type="submit" className="btn btn-neutral mt-4 mb-2">
+                Login
+              </button>
+
+              <button
+                onClick={handleGoogleLogin}
+                className="btn bg-white text-black border-[#e5e5e5]"
+              >
+                <svg
+                  aria-label="Google logo"
+                  width="16"
+                  height="16"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <g>
+                    <path d="m0 0H512V512H0" fill="#fff"></path>
+                    <path
+                      fill="#34a853"
+                      d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                    ></path>
+                    <path
+                      fill="#4285f4"
+                      d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                    ></path>
+                    <path
+                      fill="#fbbc02"
+                      d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                    ></path>
+                    <path
+                      fill="#ea4335"
+                      d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                    ></path>
+                  </g>
+                </svg>
+                Login with Google
+              </button>
+
+              <p className="text-center mt-2">
+                Do You Haven't Any Account? Pls{" "}
+                <Link to="/register" className="text-blue-500">
+                  Register
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </motion.div>
   )
 }
 
