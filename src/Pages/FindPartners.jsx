c3fe88570d9bdad39f97a6b83a45981e558bf891
robiulhasanmumin <ot7mumin@gmail.com>
2025-11-16 15:57:19 +0600

    Search functionality done

--- src/Pages/FindPartners.jsx
+++ src/Pages/FindPartners.jsx
@@ -6,20 +6,33 @@ import { CiSearch } from "react-icons/ci";
 
 const FindPartners = () => {
     const allPartners = useLoaderData()
-    console.log(allPartners);
     const [search, setSearch] = useState("");
     const [loading, setLoading] = useState(false)
+    const [sort,setSort] = useState('low')
 
-    const filterData = allPartners.filter(partner=>partner.subject.toLowerCase().includes(search.toLowerCase()))
-
+    
     const handleSearch = (e)=>{
       setSearch(e.target.value)
       setLoading(true)
       setTimeout(()=>{
         setLoading(false)
       },400)
-
+      
     }
+    
+    let filterData = allPartners.filter(partner=>partner.subject.toLowerCase().includes(search.toLowerCase()))
+
+
+    const handleSort=(type)=>{
+      setSort(type)
+    }
+          
+    if(sort==="low"){
+        filterData = [...filterData].sort((a,b)=>a.rating - b.rating)
+      } else if(sort ==="high"){
+        filterData = [...filterData].sort((a,b)=>b.rating - a.rating)
+      }
+
 
   return (
     <div className='py-8 lg:px-10 px-5'>
@@ -27,7 +40,16 @@ const FindPartners = () => {
 
 
       <div className='flex lg:flex-row flex-col gap-3 justify-between items-center mb-5 mt-5'>
-        <h2 className='text-xl font-bold'>Our Partners : <span className='text-[#4F959D]'>{filterData.length}</span></h2>
+        <div>
+         <h2 className='text-xl font-bold'>Total Partners : <span className='text-[#4F959D]'>{filterData.length}</span></h2>
+
+        <label className='text-[#4F959D] font-bold mt-2'>Sort By Rating : </label>
+        <label className='font-bold'>Low</label>
+        <input type="radio" onClick={()=>handleSort("low")} name="radio-4" className="radio w-5 h-5 ml-1 mr-2 radio-primary" defaultChecked />
+        <label className='font-bold'>High</label>
+        <input type="radio" onClick={()=>handleSort("high")} name="radio-4" className="radio ml-1 w-5 h-5 radio-primary" />
+        </div>
+
         <label className='border-2 flex items-center gap-2 border-gray-500 px-3 py-2 rounded-md'>
           <CiSearch className='right-0 top-2' />
           <input type="search" className='outline-0' value={search} placeholder='Search By Subject' onChange={handleSearch} />
